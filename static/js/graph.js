cbaa42c9a15206c3c0c753fd5b8ef8151b82f8b6
ivan <ivan@ivan-notebook.(none)>
2013-01-20 18:33:20 +0100

    initial version -0.1

---
+++ static/js/graph.js
@@ -0,0 +1,149 @@
+$(function() {
+    var socket = io.connect('/graph');
+    var $interval = $('#interval_input');
+    var $pauseButton=$('#pause_btn');
+    var paused=false;
+    var datalen = 100;
+    var interval=1;
+    
+    var kbFormatter=function(val, axis) {
+    	if (val>=1000) {
+    	return (val/1000).toFixed(axis.tickDecimals+1) + " MB/s";
+    	} else {
+    	return (val).toFixed(axis.tickDecimals) + " kB/s";
+    	}
+    };
+    var pctFormatter=function(val, axis) {
+    	return (val).toFixed(axis.tickDecimals) + " %";
+    }
+    var graphOptions={
+                xaxis:{
+                    mode: "time",
+                    timeformat: "%H:%M:%S",
+                    minTickSize: [1, "minute"],
+                },
+                yaxis: {
+                    min: 0,
+                },
+                series : {
+        				label: null,
+        				lines: { 
+            			show: true,
+            			fill: false,
+            	shadowSize: 0
+        			},
+        			points: {
+            		show:false
+        			}},
+           };
+           
+    var graphs={ cpu_graph:{
+    							series: [{key:'cpu', color:'green'}], 
+    							options:$.extend(true, {}, graphOptions, {yaxis: {
+                    max: 100, tickFormatter:pctFormatter }})},
+    						 mem_graph: {
+    						 	series: [{key:'mem', label:"Memory", color:'green'},
+    						 						{key:'swap', label:"Swap", color:'pink'}],
+    						 	options:$.extend(true, {}, graphOptions, {yaxis: {
+                    max: 100, tickFormatter:pctFormatter }})
+    						 },
+    						 disk_graph: {
+    						 	series:[{key:"disk_r", label: "Read", color:'blue'},
+    						 					{key:"disk_w", label: "Written", color:"red"}],
+    						 					options:$.extend(true, {}, graphOptions,
+    						 						{yaxis:{max:null, tickFormatter:kbFormatter}} )
+    						 },
+    						 net_graph: {
+    						 	series:[{key:"net_r", label: "Received", color:'blue'},
+    						 					{key:"net_w", label: "Sent", color:"red"}],
+    						 					options:$.extend(true, {}, graphOptions,
+    						 						{yaxis:{max:null, tickFormatter:kbFormatter}} )
+    						 },
+    					}
+    
+    var updateGraph= function( graphId, newData) {
+    	  var graph=graphs[graphId],
+    	  series=[]
+    	  var initData=function(s) {
+    	  	s.data=[]
+    	  	var ts=new Date().getTime();
+    	  	for (var i=datalen-1; i>=0; i-=1) {
+    	  		s.data[i]=[ts,0];
+    	  		ts-=interval*1000;
+    	  	}
+    	  }
+    	  
+    	  for (var i=0;i<graph.series.length;i+=1) {
+    	  	var serie=graph.series[i];
+    	  	if (!serie.data) {
+    	  		initData(serie)
+    	  	}
+    	  	serie.data.push([newData.x, newData[serie.key]]);
+        	while (serie.data.length > datalen) {
+            serie.data.shift();
+        		}
+    	  }
+    	  
+    		
+        if(graph.plot) {
+            graph.plot.setData(graph.series);
+            graph.plot.setupGrid();
+            graph.plot.draw();
+        } else  {
+            graph.plot = $.plot($('#'+graphId), graph.series, graph.options);
+            //graph.plot.draw();
+        }
+    	
+    };
+    
+    $pauseButton.click(function(){
+    	var that=$(this);
+    	if (paused) {
+    		socket.emit('cont');
+    		that.val('Pause');
+    		paused=false;
+    		$interval.removeAttr('disabled')
+    	} else {
+    		socket.emit('stop');
+    		that.val('Resume');
+    		paused=true;
+    		$interval.attr('disabled', true)
+    	};
+    	
+    });
+    socket.on('data', function(data) {
+        //console.log("Data", data.toSource())
+        for (var key in graphs) {
+        	if (graphs.hasOwnProperty(key)) {
+        updateGraph(key, data  );
+        }
+        }
+        
+    });
+    socket.on('connect', function(evt) {
+        $('#conn_status').html('<b>Connected</b>');
+        socket.emit('ready');
+        $interval.val(1);
+        $interval.change(function(evt){
+        	var new_interval=parseInt($interval.val())
+        	if (new_interval<1 ) {
+        		new_interval=1
+        	} else if (new_interval>3600) {
+        		new_interval=3600
+        	};
+        		
+        	socket.emit('change_interval', new_interval);
+        	$interval.val(new_interval);
+        	interval=new_interval
+        })
+    });
+    socket.on('error', function(name, desc) {
+        $('#conn_status').html('<b>Error</b>');
+        alert('Error '+name+'\n'+desc)
+        $internal
+    });
+    socket.on('disconnect', function(evt) {
+        $('#conn_status').html('<b>Closed</b>');
+    })
+});
+
---
+++ webresmon.py
@@ -0,0 +1,168 @@
+#/usr/bin/env python
+__version__='0.1'
+
+import os
+import time
+import paste.urlparser
+import gevent
+from socketio import socketio_manage
+from socketio.server import SocketIOServer
+from socketio.namespace import BaseNamespace
+import psutil
+
+PORT=8000
+MAX_SOCKETS=3
+
+class Sender(object):
+    def __init__(self, controler, interval):
+        self.controler=controler
+        self.running=False
+        self.glet=None
+        self.interval=interval
+        
+    def _run(self):
+        self.running=True
+        prev_net=None
+        prev_disk=None
+        def io_stats():
+            def to_kilos(x):
+                return round(x/(self.interval*1000.0),1)
+            d=[psutil.disk_io_counters().read_bytes, psutil.disk_io_counters().write_bytes]
+            n=[psutil.network_io_counters().bytes_recv, psutil.network_io_counters().bytes_sent]
+            return map(to_kilos,d),map(to_kilos,n)
+        while self.running:
+            
+            t= time.time()
+            if not prev_net or not prev_disk:
+                net=[0,0]
+                disk=[0,0]
+                prev_disk,prev_net=io_stats()
+            else:
+                new_disk, new_net=io_stats()
+                disk=map(lambda a,b: a-b, new_disk, prev_disk)
+                net=map(lambda a,b: a-b, new_net, prev_net)
+                prev_disk=new_disk
+                prev_net=new_net
+                
+            cpu_pct=psutil.cpu_percent()
+            mem_pct=psutil.virtual_memory().percent
+            swap_pct=psutil.swap_memory().percent
+            self.controler.emit_to_active('data', self.interval, {'x':t*1000, 'cpu':cpu_pct, 'mem':mem_pct, 
+            'swap':swap_pct, 'disk_r':disk[0], 'disk_w':disk[1], 'net_r':net[0], 'net_w':net[1]}
+            )
+            waited=time.time()-t
+            while waited<self.interval:
+                gevent.sleep(self.interval-waited)
+                waited=time.time()-t
+            #print str(id(self))+'-'+str( waited)
+                
+    def start(self):
+        self.glet=gevent.spawn(self._run)
+            
+    def stop(self):
+        self.running=False
+        if self.glet:
+            self.glet.kill()
+        self.glet=None
+        
+
+class GraphControler(BaseNamespace):
+    active=set()
+    sender={}
+    def initialize(self):
+        self.running=False
+        self.interval=1
+        
+    def _activate(self):
+        GraphControler.active.add(self) 
+        if not GraphControler.sender.get(self.interval):
+            GraphControler.sender[self.interval]=Sender(self, self.interval)   
+            GraphControler.sender[self.interval].start()
+            
+    def _deactivate(self):
+        def listening_on_interval(i):
+            for ctl in GraphControler.active:
+                if ctl.interval==i:
+                    return True
+        try:
+            GraphControler.active.remove(self)
+        except KeyError:
+            pass
+        if GraphControler.sender.get(self.interval) and not listening_on_interval(self.interval):
+            GraphControler.sender[self.interval].stop()
+            del GraphControler.sender[self.interval]
+            
+    def on_change_interval(self, new_interval):
+        if new_interval==self.interval:
+            return
+        try:
+            new_interval=int(new_interval)
+        except ValueError:
+            return
+        if new_interval<1 or new_interval>3600:
+            return
+        self._deactivate()
+        self.interval=new_interval
+        self._activate()
+            
+    def on_ready(self):
+        sockets_connected=len(self.socket.server.sockets)
+        print 'Ready %s' % sockets_connected
+        if sockets_connected> MAX_SOCKETS:
+            self.error("MAX_CONNECTIONS_REACHED", "Maximal limit of connections reached")
+            self.disconnect()
+            return
+        self._activate()
+        
+    def on_stop(self):
+        print 'Stop'
+        self._deactivate()
+        
+    def on_cont(self):
+        print "Continue"
+        self._activate()
+            
+    def disconnect(self, *args, **kwargs):
+        self._deactivate()
+        super(GraphControler, self).disconnect(*args, **kwargs)
+        
+    def emit_to_active(self, evt, interval, *args):
+        for ctl in self.active:
+            if ctl.interval==interval:
+                ctl.emit(evt, *args)
+        
+        
+class App(object):
+    def __init__(self):
+        self.static_handler=paste.urlparser.StaticURLParser(os.path.dirname(__file__))
+        
+        
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'].startswith('/socket.io'):
+            return socketio_manage(environ, { '/graph': GraphControler })
+        elif environ['PATH_INFO']=='/':
+            environ['PATH_INFO']='/static/graph.html'
+            return self.static_handler(environ, start_response)
+        elif environ['PATH_INFO'].startswith('/static/'):
+            return self.static_handler(environ, start_response)
+
+def main():
+    
+   
+    http_server = SocketIOServer(
+        ('', PORT),App(), 
+        policy_server=False, resource='socket.io'
+        )
+    # Start the server greenlets
+    http_server.serve_forever()
+    
+    
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
